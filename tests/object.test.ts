a0b4ebada2b346e5e7dc4268a28e0412e0c673af
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-30 11:17:00 +0700

    Object Type - video 15

---
+++ tests/object.test.ts
@@ -0,0 +1,18 @@
+describe('Object', function () {
+    it('should support in typescript', function () {
+        
+        // deklarasi objek
+        const person: {id: string, name: string} = {
+            id: "1",
+            name: "Abil"
+        }
+
+        console.info(person)
+
+        person.id = "2";
+        person.name = "Fadilah"
+
+        console.info(person)
+
+    })
+})
