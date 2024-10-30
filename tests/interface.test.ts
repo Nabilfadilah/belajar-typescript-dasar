0e7f12819a7e254502249dda0658862ce30793d4
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-30 15:12:29 +0700

    Function di interface - video 23

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -77,4 +77,23 @@ describe('Interface', function () {
 
         console.info("ini Data Manager : ", manager)
     })   
+
+    // Function di interface
+    it('should support function in interface', function () {
+        
+        interface Person {
+            name: string;
+            sayHello(name: string) :string;
+        }
+
+        const person: Person = {
+            name: "Nabill",
+            sayHello: function (name: string): string {
+                return `Hello ${name}, my name is ${this.name}`
+            },
+        }
+
+        console.info(person.sayHello("Ramsayyy"))
+    
+    })
 })
