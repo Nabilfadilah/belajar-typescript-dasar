c3a5d73702db0a8c821e98ffefd7cba27265487d
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-30 21:11:01 +0700

    Function Parameter - video 27

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -12,7 +12,44 @@ describe('Function', function () {
         }
 
         printHello("Ramsay")
-
     })
 
+    // function parameter
+    it('should support in typescript', function () {
+        // kalau mau tambahkan default value contohnya ("Guest")
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Nabil")).toBe("Hello Nabil");
+    })
+
+    // sum function
+    it('should support rest parameter', function () {
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        } 
+
+        expect(sum(1,2,3,4,5)).toBe(15);
+    })
+
+    it('should support optional parameter', function () {
+        function sayHello(firstName: string, lastName? : string): string {
+            if(lastName) {
+                return `Hello ${firstName} ${lastName}`
+            } else {
+                return `Hello ${firstName}`
+            }
+           
+        }
+
+        expect(sayHello("Gordon")).toBe("Hello Gordon")
+        expect(sayHello("Gordon", "Fadilah")).toBe("Hello Gordon Fadilah")
+    
+    })
 })
