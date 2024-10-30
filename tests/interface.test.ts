a7491d4449630eac793a3abc06e55cb16c50ddbe
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-30 14:08:52 +0700

    Function Interface - video 20

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -13,6 +13,21 @@ describe('Interface', function () {
         seller.name = "Ian None"
 
         console.info(seller)
+    })
+
+    // function interface 
+    it('should support function interface', function () {
+        
+        interface addFunction {
+            (value1: number, value2: number): number;
+        }
+
+        const add: addFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        }
+
+        expect(add(2,2)).toBe(4);
 
     })
+         
 })
