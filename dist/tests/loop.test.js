19f1a2bfedca6de0aa03b69f871276cf54bb9c5d
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-31 09:38:54 +0700

    Do While Loop - video 35

--- dist/tests/loop.test.js
+++ dist/tests/loop.test.js
@@ -1,44 +1,35 @@
+"use strict";
 describe('Loop', function () {
     // for loop
     it('should support for Loop', function () {
-    
-        const names : string[] = ["Mohammad", "Nabil", "Fadilah"]
-
+        const names = ["Mohammad", "Nabil", "Fadilah"];
         // for i
         for (let i = 0; i < names.length; i++) {
-            console.info(names[i])
+            console.info(names[i]);
         }
-
         // fot of
         for (const name of names) {
-            console.info(name)
+            console.info(name);
         }
-
         // for in
         for (const index in names) {
-            console.info(names[index])
+            console.info(names[index]);
         }
-    })
-
+    });
     // while loop - video 34
     it('should support while loop', function () {
-
-        let counter:number = 0;
-
+        let counter = 0;
         while (counter < 10) {
             console.info(counter);
             counter++;
         }
-    })
-
+    });
     // Do while loop - video 35
     it('should support do while loop', function () {
-        
-        let counter:number = 0;
-
+        let counter = 0;
         do {
-            console.info("ini do while loop" ,counter);
+            console.info("ini do while loop", counter);
             counter++;
-        } while (counter < 10)
-    })
-})
+        } while (counter < 10);
+    });
+});
